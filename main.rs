dc51e1a19221cf004c14194accd6eac529e5acea
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-02-20 23:23:12 +0700

    first commit

---
+++ main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let custom_num = 98_000;
+    let hex_num = 0xfa;
+    let bin_num = 0b0010_1011;
+    let byte_num = b'A';
+
+    println!("{}", custom_num);
+    println!("{}", hex_num);
+    println!("{}", bin_num);
+    println!("{}", byte_num);
+}
